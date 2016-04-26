bdbda865fc0f1a38d3b67c169ef17efea4b6c481
Thiste <thiste2505@googlemail.com>
2016-04-26 07:17:51 +0200

    Kommentare hinzugefuegt.

--- src/DoubleLinkedList.java
+++ src/DoubleLinkedList.java
@@ -1,54 +1,90 @@
+//TODO @Override Annotationen richtig?!
+//TODO length überprüfen
+
+/**
+*
+* Doppelt verkettete Liste
+*
+* @author  
+* @author Maximilian von Unwerth 4568393 Group ?
+*/
 public class DoubleLinkedList implements List {
-	private static Pokemon head = new Pokemon();
+	
+	/**
+	* Kopf der Liste
+	*/
+	private static Pokemon head = new Pokemon(); 
+	
+	/**
+	* Schwanz der Liste
+	*/
 	private static Pokemon tail = new Pokemon();
 	
+	/**
+	* Konstruktor der Liste
+	*/
 	public DoubleLinkedList() {
 		head.setNext(tail);
 		tail.setPrev(head);
 	}
 	
+	/**
+	* Die Liste ist leer, wenn das erste Element das letzte ist.
+	*/
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
 		return head.next() == tail;
 	}
 
+	/**
+	* 
+	*/
 	@Override
 	public int length() {
 		int l = 0;
-		Pokemon p = head.next();
-		
-		while(p != tail) {
+		Pokemon p = head.next();	
+		//Fuer jedes Element in der Liste, 
+		while(p != tail) { 
 			l++;
 			p = p.next();
 		}
 		return l;
 	}
 
+	/**
+	* Gibt den Kopf der Liste zurueck
+	* @return Das erste Pokemon 
+	*/
 	@Override
 	public Pokemon firstPokemon() {
 		return head;
 	}
 
+	/**
+	* Fuegt ein Pokemon in die Liste ein
+	* @param Einzufuegendes Pokemon
+	*/
 	@Override
 	public void insert(Pokemon p) {
 		p.setNext(tail);
 		p.setPrev(tail.prev());
 		tail.prev().setNext(p);
 		tail.setPrev(p);
-                tail.prev().next().toString();
+        tail.prev().next().toString();
 	}
 
+	/**
+	* Loescht ein Pokemon aus der Liste
+	*/
 	@Override
 	public void delete(Pokemon p) {
 		Pokemon x = head;
-		Pokemon y = head.next();
-		
+		Pokemon y = head.next();		
 		while(y != tail && y != p) {
 			x = x.next();
 			y = y.next();
 		}
-		
 		if(y != tail) {
 			x.setNext(y.next());
 			x.next().setPrev(y.prev());
@@ -56,25 +92,31 @@ public class DoubleLinkedList implements List {
 		
 	}
 	
+	/**
+	* Gibt die Liste als Stirng zurueck
+	* @return Die Liste als String
+	*/
 	public String toString() {
 		Pokemon p = head;
 		String s = "";
 		while(p.next() != tail) {
                     p = p.next();         
                     s += p.toString() + "\n";
-		}
-		
+		}		
 		return s;
 	}
 	
+	/**
+	* Fuegt ein Pokemon an die richtige Position ein
+	* @param Einzufuegenden Pokemon
+	*/
 	public void insertSorted(Pokemon p) {
 		Pokemon x = head;
 		Pokemon y = head.next();
 		while(y != tail && p.getNr() >= y.getNr() ) {
 			x = x.next();
 			y = y.next();
-		}
-		
+		}		
 		p.setPrev(x);
 		p.setNext(y);
 		x.setNext(p);
--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,24 +1,91 @@
+//TODO Total aus der Summe aller Werte berechnen?!
+//TODO public Pokemon() Konstruktor noetig? --> nullPkm ruft Konstruktor mit Parametern auf
+
+/**
+* @author  
+* @author Maximilian von Unwerth 4568393 Group ?
+*/
 public class Pokemon {
 	
-	
+	/**
+	* Das vorherige Element der Liste
+	*/
 	private Pokemon prev;
-	private Pokemon next;
-	protected static Pokemon nullPkm = new Pokemon(-1, "", "", "", 0, 0, 0, 0, 0, 0, 0);
 	
+	/**
+	* Das naechste Element der Liste
+	*/
+	private Pokemon next;
+	
+	/**
+	* Das Null-Pokemon
+	*/
+	protected static Pokemon nullPkm = new Pokemon(-1, "", "", "", 0, 0, 0, 0, 0, 0, 0);
+
+	/**
+	* Die Nummer des Pokemons
+	*/
 	private int nr;
+	
+	/**
+	* Der Name des Pokemons
+	*/
 	private String name;
+	
+	/**
+	* Typ1 des Pokemons
+	*/
 	private String type1;
+	
+	/**
+	* Typ2 des Pokemons
+	*/
 	private String type2;
+	
+	/**
+	* Die Summe aller Werte des Pokemons
+	*/
 	private int total;
+	
+	/**
+	* Die Kraftpunkte des Pokemons
+	*/
 	private int hp;
+
+	/**
+	* Der Angriffswert des Pokemons
+	*/
 	private int atk;
+	
+	/**
+	* Der Verteidigungswert des Pokemons
+	*/
 	private int def;
+	
+	/**
+	* Der Spezialangriffswert des Pokemon
+	*/
 	private int spatk;
+	
+	/**
+	* Der Spezilverteidigungswert des Pokemon
+	*/
 	private int spdef;
+	
+	/**
+	* Der Geschwindigkeitswert des Pokemon
+	*/
 	private int speed;
 	
+	/**
+	* Die Anzahl der maximalen Zeichen eines Namen, bzw. eines Typs
+	* fuer das korrekt gezeichnete Layout
+	*/
 	private final int LONGEST_STRING = 10;
-	
+
+	/**
+	* NULL-Pokemon-Konstruktor
+	*/
 	public Pokemon() {
 		this.nr=-1;
 		this.name="";
@@ -32,6 +99,9 @@ public class Pokemon {
 		this.speed=0;
 	}
 	
+	/**
+	* Standard Pokemon-Konstruktor
+	*/
 	public Pokemon( int nr,String name, String type1, String type2,
 			int total,int hp,int atk,int def,int spatk,
 			int spdef,int speed) {
@@ -48,10 +118,17 @@ public class Pokemon {
 		this.speed=speed;
 	}
 	
+	/**
+	* Getter fuer die Nummer des Pokemon
+	*/
 	public int getNr() {
 		return nr;
 	}
 	
+	/**
+	* Die Nummer des Pokemon auf 3 Zeichen Laenge bringen
+	* Fuer das Tabellenlayout
+	*/
 	private String convertNumber(int num) {
 		String sNr;
 		if(num < 10) {
@@ -67,6 +144,12 @@ public class Pokemon {
 		return sNr;
 	}
 	
+	/**
+	* String auf die maximale Laege eines Strings bringen
+	* Fuer das Tabellenlayout mit Leerzeichen fuellen
+	* @param Name, Typ... der formatiert werden soll
+	* @return Formatierter String auf maximale Laenge
+	*/
 	private String convertString(String str) {
 		String sName = "";
 		for(int i = 0; i < LONGEST_STRING - str.length(); i++) {
@@ -77,30 +160,43 @@ public class Pokemon {
 		return sName;
 	}
 	
+	/**
+	* An vorderes Element ankoppeln
+	* @param vorderes Pokemon
+	*/
 	public void setPrev(Pokemon pkm) {
 		this.prev = pkm;
 	}
 	
+	/**
+	* Vorheriges Element zurueckgeben
+	* @return Vorheriges Pokemon
+	*/
 	public Pokemon prev() {
-	/*	if(prev != DoubleLinkedList.head) {
-			return prev;
-		}
-		return new Pokemon();*/
 		return prev;
 	}
 	
+	/**
+	* Nachfolgendes Element setzen
+	* @param Naechstes Pokemon
+	*/
 	public void setNext(Pokemon pkm) {
 		this.next = pkm;
 	}
 	
+	/**
+	* Naechstes Pokemon setzen
+	* @return Nachfolgendes Pokemon
+	*/
 	public Pokemon next() {
-		/*if(next != DoubleLinkedList.tail) {
-			return next;
-		}
-		return new Pokemon();*/
 		return next;
 	}
 	
+	/**
+	* Werte in Strings umwandeln und 
+	* kompletten Eintrag als Reihe speichern
+	* @return Formatierte Reihe
+	*/
 	public String toString() {
 		String sNr = convertNumber(nr);
 		String sName = convertString(name);
@@ -113,10 +209,6 @@ public class Pokemon {
 		String sSpAtk = convertNumber(spatk);
 		String sSpDef = convertNumber(spdef);
 		String sSpeed = convertNumber(speed);
-		
-		
-		
-		
 		String formated_line = sNr + " | " + sName + " | " + sType1 + " | " + sType2 + " | " + sTotal + " | " + sHp + " | " + sAtk + " | " + sDef + " | " + sSpAtk + " | " + sSpDef + " | " + sSpeed;
 		return formated_line;
 	}
